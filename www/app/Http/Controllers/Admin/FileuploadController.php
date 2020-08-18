00e36d87ab820eab8cf3b5b201b7d778f4dacefb
iwanaga <nagaiwasann@gmail.com>
2020-08-18 20:35:40 +0900

    update image file upload logic

--- www/app/Http/Controllers/Admin/FileuploadController.php
+++ www/app/Http/Controllers/Admin/FileuploadController.php
@@ -9,29 +9,21 @@ use Illuminate\Support\Facades\Auth;
 class FileuploadController extends Controller
 {
   public function upload(Request $request) {
-    // sessionからTokenを取得
-    $token = session('file_upload_token');
     // 基本のResponseを作成
     $data = collect([
       'uploaded' => false,
     ]);
-    if(empty($token)) {
-      $data = $data->merge(['error' => [
-        'message' => 'error no token'
-      ]]);
-    } else {
 
-      //バリデーションされているファイルは (jpeg, png, bmp, gif, or svg)
-      //3Mb以下のファイル
-      $validation = \Validator::make($request->all(), ['upload' => 'image|max:30000']);
+    //バリデーションされているファイルは (jpeg, png, bmp, gif, or svg)
+    //3Mb以下のファイル
+    $validation = \Validator::make($request->all(), ['upload' => 'image|max:30000']);
 
-      // バリデーションチェックを行う
-      if (!$validation->fails()) {
-        $fileName = $request->file('upload')->getClientOriginalName();
-        $savePath = 'public/'.Auth::id();
-        $path = $request->file('upload')->storeAs($savePath, $fileName, ['public']);
-        $data = $data->merge(['uploaded'=> true, 'url' => \Storage::url($path)]);
-      }
+    // バリデーションチェックを行う
+    if (!$validation->fails()) {
+      $fileName = $request->file('upload')->getClientOriginalName();
+      $savePath = 'public/'.Auth::id();
+      $path = $request->file('upload')->storeAs($savePath, $fileName, ['public']);
+      $data = $data->merge(['uploaded'=> true, 'url' => \Storage::url($path)]);
     }
 
     // 配列で返すとHeader付きjsonになる
